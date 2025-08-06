51e2cb830b661246273fe20e1ee913df9899890a
Emilie Pessoa Rodrigues <emiliepessoarodrigues@gmail.com>
2025-08-05 22:06:55 -0300

    Criando e aprendendo com o React JS

---
+++ introreact/src/App.jsx
@@ -0,0 +1,56 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import MotivosParaUsarReact from './components/MotivosParaUsarReact'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+
+      <h1>Página de React JS</h1>
+
+      <MotivosParaUsarReact />
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+
+{/* <div>
+  <a href="https://vite.dev" target="_blank">
+    <img src={viteLogo} className="logo" alt="Vite logo" />
+  </a>
+  <a href="https://react.dev" target="_blank">
+    <img src={reactLogo} className="logo react" alt="React logo" />
+  </a>
+</div>
+<h1>Vite + React</h1>
+<div className="card">
+  <button onClick={() => setCount((count) => count + 1)}>
+    count is {count}
+  </button>
+  <p>
+    Edit <code>src/App.jsx</code> and save to test HMR
+  </p>
+</div>
+<p className="read-the-docs">
+  Click on the Vite and React logos to learn more
+</p> */}
---
+++ introreact/src/components/MotivosParaUsarReact.jsx
@@ -0,0 +1,12 @@
+// usar rfce pra puxar de uma vez
+// import React from "react"; // isso n precisa mais, da de tirar
+
+import './MotivosParaUsarReact.css'
+
+function MotivosParaUsarReact() {
+  return (
+    <p className="motivos">Um componente criado pode ser reutilizado.</p>
+  )
+}
+
+export default MotivosParaUsarReact
